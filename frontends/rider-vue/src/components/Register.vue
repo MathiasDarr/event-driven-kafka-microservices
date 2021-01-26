baff7854bfb5aad26978d458d41cbfbc19bfe781
mddarr <mddarr@gmail.com>
2021-01-25 16:26:44 -0800

    register from the front end..

--- frontends/rider-vue/src/components/Register.vue
+++ frontends/rider-vue/src/components/Register.vue
@@ -10,7 +10,7 @@
             <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
             <v-text-field v-model="password" :rules="[rules.required, rules.min]" label="Password" required></v-text-field>
             <v-text-field v-model="verify" :rules="[rules.required, passwordMatch]" label="Verify Password" required></v-text-field>
-            <v-btn class="error mt4" @click="authenticate()" > Register </v-btn>
+            <v-btn class="error mt4" @click="register()" > Register </v-btn>
             <p>Already have an account? - <router-link to="Login">Login Now</router-link></p> 
          </v-form>
         
@@ -18,6 +18,11 @@
 </template>
 
 <script>
+
+import router from '../router'
+import axios from 'axios'
+
+
 export default {
   name: 'Register',
   computed:{
@@ -25,11 +30,14 @@ export default {
       return () => this.password === this.verify || "Password must match";
     }
   },
+
+
+  
   data() {
       return {
-            firstname: 'Charles',
-            lastname: 'Jones',
-            email: 'cjones@gmail.com',
+            firstname: 'Erik',
+            lastname: 'Daniels',
+            email: 'demda@gmail.com',
             password: '1!ZionTF',
             verify:'1!ZionTF',
             error: null,
@@ -49,6 +57,40 @@ export default {
       }
   },
   methods: {
+
+    async async_register(){
+        try{
+            this.loading = true
+                /*eslint no-unused-vars: "off"*/
+
+            var url = "http://localhost:8096/api/auth/register"
+            var body = {email:this.email, password:this.password, firstname:this.firstname, lastname: this.lastname}
+
+            axios.post(url, body).then((response) => {
+                console.log(response.data.email)
+                router.push({path:"/login"})
+
+            }, (error) => {
+                    
+            console.log(error);
+            this.loading = false
+            });
+                
+        }catch(err){
+            console.log(err)
+        }
+    },
+
+
+
+    async register () {
+        
+        this.async_register()
+
+
+    },
+
+
     authenticate () {
         /*eslint no-unused-vars: "off"*/
         this.$cognitoAuth.signup(this.email,this.firstname, this.lastname, this.password, (err, result) => {
--- microservices/rest-service-cors/src/main/java/org/mddarr/ride/request/services/controllers/AuthController.java
+++ microservices/rest-service-cors/src/main/java/org/mddarr/ride/request/services/controllers/AuthController.java
@@ -2,8 +2,11 @@ package org.mddarr.ride.request.services.controllers;
 
 import org.mddarr.ride.request.services.dto.auth.AuthenticationResponse;
 import org.mddarr.ride.request.services.dto.auth.LoginRequest;
+import org.mddarr.ride.request.services.dto.auth.RegisterRequest;
 import org.mddarr.ride.request.services.services.auth.AuthorizationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,6 +15,14 @@ public class AuthController {
 
     @Autowired
     AuthorizationService authorizationService;
+
+    @PostMapping("/register")
+    @CrossOrigin(origins = "http://localhost:8080", allowedHeaders = "*", exposedHeaders = "If-Match")
+    public ResponseEntity register(@RequestBody RegisterRequest registerRequest){
+        authorizationService.register(registerRequest);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
 
     @PostMapping("/login")
     @CrossOrigin(origins = "http://localhost:8080", allowedHeaders = "*", exposedHeaders = "If-Match")
